f076ef1c43ebbcabd8fe6d20c9908659e22df44d
Emilia <correoalcantaraemilia@gmail.com>
2023-07-26 20:24:10 -0400

    Agrego implementacion de navigation + 2 fragmentos.

--- app/src/main/java/cl/individual/martes250723/MainActivity.kt
+++ app/src/main/java/cl/individual/martes250723/MainActivity.kt
@@ -3,6 +3,18 @@ package cl.individual.martes250723
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/* Ejercicio 1 - Módulo 6
+ * Recycler view
+ * ROOM
+ * - dao
+ * - base datos
+ * - entity
+ * Crear Interfaz (para poder agregar una nueva tarea)
+ * Fragmentos
+ * - uno para crear y
+ * - otro para listar
+ *
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
